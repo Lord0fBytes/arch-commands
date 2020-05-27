e7fee45fa1170e39bc702f989120abbba4520119
Justin Masse <jmasse@webtron.me>
2020-05-27 15:34:54 -0500

    upadted template with help and blank variables

--- .template.sh
+++ .template.sh
@@ -1,5 +1,26 @@
-#!/bin/bash/
+#!/bin/bash
 
+#COMMAND=@CMD (Change Me)
 #TITLE= Change this title
 
+#@HELP -> Displays this help menu for this command
+help() {
+	#Pulls the help files from this command
+	CMD=$(echo "@${0##*/}" | sed 's/.sh//g')
+	echo ""
+	echo "~~ $CMD Help Menu ~~"
+	echo "----------------------------------"
+	grep -e '^#@' $PWD/${0##*/} | sed 's/#@//g'
+	echo ""
+}
 
+#@BLANK -> Default command (CHANGE ME)
+blank(){
+	#This runs be default with no options
+	echo "hello"
+}
+
+case $1 in 
+	help) help ;;
+	*) blank ;;
+esac
