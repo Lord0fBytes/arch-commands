aff95f5c77864ca81eab4a09b1b370915fa9b133
Justin Masse <jmasse@webtron.me>
2020-05-29 04:22:32 -0500

    fixed the loop for connecting

--- wifi.sh
+++ wifi.sh
@@ -30,8 +30,16 @@ status() {
 
 #@CONNECT -> Forces the wireless NIC to reconnect
 connect() {
-	sudo wpa_supplicant -B -i wlp2s0 -c /etc/wpa_supplicant/wpa_supplicant.conf 
-
+	sudo wpa_supplicant -B -i wlp2s0 -c /etc/wpa_supplicant/wpa_supplicant.conf > /dev/null 2>&1
+	echo "Connecting to SSID..."
+	wifistatus=$(sudo wpa_cli status | grep -e 'wpa_state')
+	while [ -z $(sudo wpa_cli status | grep -e 'ip_address') ] #potential infinite loop; need to add a failsafe counter after 30 seconds
+	do
+		sleep 1
+	done
+	ipaddr=$(sudo wpa_cli status | grep -e 'ip_address' | sed 's/ip_address=//g')
+	ssid=$(sudo wpa_cli status | grep -e '^ssid' | sed 's/ssid=//g')
+	echo "Connected to $ssid@$ipaddr"
 }
 
 case $1 in 
